27d1ab4cf9b27470fa02be717dc940e4df01ef09
BeatrizVRodrigues <116577946+BeatrizVRodrigues@users.noreply.github.com>
2022-11-02 11:25:36 +0100

    Add second function and updated other

--- radians to degrees.py
+++ radians to degrees.py
@@ -1,6 +1,12 @@
 
 from math import pi
 
-def radians_to_degrees(number):
+def radians_to_degrees(deg_number):
     #number should be provided in radians
+    rad_number=
+    return rad_number
     
+def degrees_to_radians(number_rad):
+    #number should be provided in degrees
+    number_deg = 
+    return number_deg
